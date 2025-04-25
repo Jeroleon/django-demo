67e272592a077cf61c6ae4aac86b95c49e001a39
jeroleon <jebamalaijero.24it@licet.ac.in>
2025-04-25 12:05:05 +0530

    fetching from user and printing back

--- main/views.py
+++ main/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def add(request):
+    name = request.GET.get('name')
+    email = request.GET.get('email')
+    phone = request.GET.get('phone')
+    address = request.GET.get('address')
+    return render(request, 'main/result.html', {'name': name, 'email': email, 'phone': phone, 'address': address})
